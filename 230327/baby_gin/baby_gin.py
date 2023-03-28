e535e24ea5144cc30afeca48b8341df9c529bb98
nexusin <nexusin@naver.com>
2023-03-28 12:18:28 +0900

    230327

--- 230327/baby_gin/baby_gin.py
+++ 230327/baby_gin/baby_gin.py
@@ -1,20 +1,41 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
-
-def perm(i, k):  # i: 값을 결정할 자리, k: 결정할 개수
-    if i==k:
-        print(*lst), card
-    else:
-        for j in range(1, k):  # 자신부터 오른쪽 원소들과 교환
-            lst[i], lst[j] = lst[j], lst[i]
-            perm(i+1, k)
-            lst[i], lst[j] = lst[j], lst[i]
-
 T = int(input())
 
-for test_case in range(1):
-    lst = [i for i in input()]
-    cards = []
-    perm(0, 6)
-    print(f'#{test_case} {cards}')
+for test_case in range(1, T + 1):
+    counter = [0 for _ in range(10)]
+    numbers = list(map(int, input()))
+    for number in numbers:
+        counter[number] += 1
+
+    is_babygin = 0
+
+    # idx를 변화시키면서 triplet 검사와 run 검사
+    idx = 0
+    while idx < len(counter):
+        # triplet(3장이 같은 수) 검사
+        if counter[idx] >= 3:
+            counter[idx] -= 3
+            is_babygin += 1
+            continue
+
+        # run(3장이 연속된 3개의 숫자) 검사
+        if idx < len(counter) - 2:
+            # counter[idx] == 0이면 bool(counter[idx] == 0) == False (Falsy)
+            if counter[idx] and counter[idx + 1] and counter[idx + 2]:
+                counter[idx] -= 1
+                counter[idx + 1] -= 1
+                counter[idx + 2] -= 1
+                is_babygin += 1
+                continue
+        if is_babygin == 2:
+            break
+
+        # 다음 카드 검증
+        idx += 1
+
+    if is_babygin == 2:
+        print(f'#{test_case} 1')
+    else:
+        print(f'#{test_case} 0')
