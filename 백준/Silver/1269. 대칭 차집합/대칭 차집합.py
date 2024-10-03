a9d84d1fea46505558fc83dc6357c682a7d78a79
dev-bleck <109737484+dev-bleck@users.noreply.github.com>
2024-10-03 23:52:09 +0900

    [Silver IV] Title: 대칭 차집합, Time: 296 ms, Memory: 211384 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -5,7 +5,7 @@ A, B = map(int, input().split())
 A_Numbs = list(map(int, input().split()))
 B_Numbs = list(map(int, input().split()))
 
-A_res = list(set(A_Numbs) - set(B_Numbs))
-B_res = list(set(B_Numbs) - set(A_Numbs))
+A_res = list(set(A_Numbs).difference(B_Numbs))
+B_res = list(set(B_Numbs).difference(A_Numbs))
 
 print(len(A_res) + len(B_res))
