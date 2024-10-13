6131b34404210ff8a5c2ca3fd3593d3e1d8e7c48
dev-bleck <109737484+dev-bleck@users.noreply.github.com>
2024-10-13 23:51:15 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+word = str(input().rstrip())
+
+if word == 'SONGDO':
+    print('HIGHSCHOOL')
+elif word == 'CODE':
+    print('MASTER')
+elif word == '2023':
+    print('0611')
+elif word == 'ALGORITHM':
+    print('CONTEST')
